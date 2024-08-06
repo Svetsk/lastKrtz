84866f87a711a57c38e1810f4894348f15448ea9
WizSol <fullempe@gmail.com>
2024-08-06 04:06:49 +0300

    последнее проверочное

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,15 +2,19 @@ export default {
   plugins: [
     '~/plugins/js/index.js'
   ],
+
   server: {
     host: '0.0.0.0'
   },
+
   devtools: { enabled: false },
   css: ["~/assets/styles/global.scss"],
   modules: ["@pinia/nuxt", "@nuxt/image", "@nuxtjs/tailwindcss", "nuxt-swiper", "@vite-pwa/nuxt"],
+
   swiper: {
     // modules: ['navigation', 'pagination', 'SwiperAutoplay', 'SwiperEffectCreative', 'SwiperControls'],
   },
+
   head: {
     meta: [
       { name: 'viewport', content: 'width=device-width, initial-scale=1' }
@@ -23,4 +27,6 @@ export default {
       middleware: 'preloader'
     }
   },
-}
+
+  compatibilityDate: '2024-08-06',
+};
